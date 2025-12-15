555ab8ca28eeda06937d60cd7986b204862f2f5f
Jay Rane <127013993+Jay-shm@users.noreply.github.com>
2025-12-15 15:08:12 +0530

    adbanner

---
+++ src/components/AdBanner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdBanner: React.FC = () => {
+    return (
+        <div style={{ border: '1px solid #ccc', padding: '20px', textAlign: 'center', margin: '20px 0' }}>
+            <p>Ad Space</p>
+        </div>
+    );
+};
+
+export default AdBanner;
