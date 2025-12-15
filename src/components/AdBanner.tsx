18602b6915f121df1a50af130814680793acfd25
Jay Rane <127013993+Jay-shm@users.noreply.github.com>
2025-12-15 17:10:13 +0530

    added

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AdBanner: React.FC = () => {
     return (
         <div style={{ border: '1px solid #ccc', padding: '20px', textAlign: 'center', margin: '20px 0' }}>
-            <p>Ad Space</p>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2598684429938450" crossOrigin="anonymous"></script>
         </div>
     );
 };
